300f61bf8400875ccb29c6ffe05abd983ce06519
Hồ Khánh Duy <111079978+voicon324@users.noreply.github.com>
2025-06-12 11:41:20 +0700

    Update base.py

--- src/newaibench/datasets/base.py
+++ src/newaibench/datasets/base.py
@@ -305,8 +305,7 @@ class BaseDatasetLoader(ABC):
         
         for query_id, judgments in qrels.items():
             if str(query_id) not in query_ids:
-                logger.warning(f"Qrel query ID not found in queries: {query_id}, {type(query_id)}, {type(query_ids[0])}")
-                pass
+                logger.warning(f"Qrel query ID not found in queries: {query_id}")
             
             for doc_id, score in judgments.items():
                 if str(doc_id) not in corpus_ids:
